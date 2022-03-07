37aa91993955847291d69756eb51313266840f07
Saeed Zareian <saeedzareian@users.noreply.github.com>
2022-03-06 22:54:07 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ from setuptools import setup
 
 setup(
     name="target-parquet",
-    version="0.2.2",
+    version="0.2.3",
     description="Singer.io target for writing into parquet files",
     author="Rafael 'Auyer' Passos",
     url="https://singer.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["target_parquet"],
     install_requires=[
-        "jsonschema==2.6.0",
-        "singer-python==5.12.1",
-        "pyarrow==4.0.1",
-        "psutil==5.8",
+        "jsonschema==4.4.0",
+        "singer-python==5.12.2",
+        "pyarrow==7.0.0",
+        "psutil==5.9.0",
     ],
     extras_require={
         'dev': [
