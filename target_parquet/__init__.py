3a73000c7da88632767ae2d606c9231fc53de986
Saeed Zareian <saeedzareian@users.noreply.github.com>
2022-06-22 09:53:09 -0400

    Update __init__.py

--- target_parquet/__init__.py
+++ target_parquet/__init__.py
@@ -166,7 +166,7 @@ def persist_messages(
 
     def write_file(current_stream_name, record):
         
-        batch_size = 15790
+        batch_size = 12790
         timestamp = datetime.utcnow().strftime("%Y%m%d_%H%M%S-%f")
         LOGGER.info(f"Writing files from {current_stream_name} stream")
         fields = extract_field_names(record)
